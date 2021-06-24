0da5c347a5fd66cca45193e926789cdc2b052160
Ahmed Joum <ahmed.moniim@gmail.com>
2021-06-24 02:23:40 +0200

    login, register, search product

--- src/main/java/com/example/demo/Product/ProductCont.java
+++ src/main/java/com/example/demo/Product/ProductCont.java
@@ -2,7 +2,7 @@ package com.example.demo.Product;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
-
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,24 +10,43 @@ import org.springframework.stereotype.Controller;
 
 import java.util.List;
 
+import com.example.demo.User.User;
+
 @Controller
-@RequestMapping(path = "/product")
+@RequestMapping(path = "/shop")
 
 public class ProductCont {
 
-    private final ProductService productService;
-
     @Autowired
-    public ProductCont(ProductService productService) {
-        this.productService = productService;
+    private ProductService productService;
+
+    @GetMapping(path = "/home")
+    public String showHomePage(@ModelAttribute("user") User user, Model model) {
+        if (user == null) {
+            return "redirect:/user/login";
+        } else {
+            model.addAttribute("user", user);
+            model.addAttribute("keyword", new SearchRequestDto());
+            return "index";
+        }
     }
 
     @PostMapping(path = "/search")
-    public String getProduct(@ModelAttribute("keyword") String keyword, 
-    Model model) {
-       
-        List<Product> products = productService.search(keyword);
+    public String searchProduct(@ModelAttribute("keyword") SearchRequestDto keyword, Model model) {
+        List<Product> products = productService.search(keyword.getKeyword());
         model.addAttribute("products", products);
         return "Shop";
+    }
+
+    public class SearchRequestDto {
+        private String keyword;
+
+        public String getKeyword() {
+            return keyword;
+        }
+
+        public void setKeyword(String keyword) {
+            this.keyword = keyword;
+        }
     }
 }
--- src/main/java/com/example/demo/User/UserCont.java
+++ src/main/java/com/example/demo/User/UserCont.java
@@ -4,8 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,18 +19,45 @@ public class UserCont {
 
     @GetMapping("/login")
     public String showLoginPage(Model model) {
-User user = new User();
-        model.addAttribute("user", user);
+        var user = model.getAttribute("user");
+        if (user == null) {
+            user = new User();
+            model.addAttribute("user", user);
+        }
         return "login";
     }
 
-    @PostMapping(path =  "/validateLogin")
-    public String validateLogin(@ModelAttribute("user") User user) {
+    @PostMapping(path = "/validateLogin")
+    public String validateLogin(@ModelAttribute("user") User user, RedirectAttributes redir) {
         var loginUser = userService.login(user.getUserName(), user.getPassword());
-        if (loginUser.equals(null)) {
-            return "/login";
+        redir.addFlashAttribute("user", user);
+        if (loginUser == null) {
+            redir.addFlashAttribute("error", "Invalid Username or password");
+            return "redirect:/user/login";
         } else {
+            if (loginUser.getType() == 1) {
+                return "redirect:/shop/home";
+            } else {
+                return "index"; // return admin dashboard
+            }
+        }
+    }
+
+    @GetMapping("/register")
+    public String showRegistrationPage(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
+        return "register";
+    }
+
+    @PostMapping("/saveUser")
+    public String saveUser(@ModelAttribute("user") User user) {
+        user.setType(1);
+        var isRegestered = userService.register(user);
+        if (isRegestered) {
             return "index";
+        } else {
+            return "register";
         }
     }
 
